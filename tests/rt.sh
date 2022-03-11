3cb401a2ffa7b5797818ac11e39f1cbd9ec586d6
snoaa <97476595+snoaa@users.noreply.github.com>
2022-03-11 15:48:05 -0500

    Update rt.sh

--- tests/rt.sh
+++ tests/rt.sh
@@ -485,7 +485,7 @@ if [[ $TESTS_FILE =~ '35d' ]] || [[ $TESTS_FILE =~ 'weekly' ]]; then
   TEST_35D=true
 fi
 
-BL_DATE=22222222
+BL_DATE=99999999
 if [[ $MACHINE_ID = hera.* ]] || [[ $MACHINE_ID = orion.* ]] || [[ $MACHINE_ID = cheyenne.* ]] || [[ $MACHINE_ID = gaea.* ]] || [[ $MACHINE_ID = jet.* ]] || [[ $MACHINE_ID = s4.* ]]; then
   RTPWD=${RTPWD:-$DISKNM/NEMSfv3gfs/develop-${BL_DATE}/${RT_COMPILER^^}}
 else
@@ -493,7 +493,7 @@ else
 fi
 
 INPUTDATA_ROOT=${INPUTDATA_ROOT:-$DISKNM/NEMSfv3gfs/input-data-33333333}
-INPUTDATA_ROOT_WW3=${INPUTDATA_ROOT}/WW3_input_data_20211113
+INPUTDATA_ROOT_WW3=${INPUTDATA_ROOT}/WW3_input_data_55555555
 INPUTDATA_ROOT_BMIC=${INPUTDATA_ROOT_BMIC:-$DISKNM/NEMSfv3gfs/BM_IC-20220207}
 
 shift $((OPTIND-1))
